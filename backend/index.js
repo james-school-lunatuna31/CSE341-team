c005ea5a4876626026a8474ef4d74b8b43a799e8
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-05-19 11:26:48 -0700

    More framing for development

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,11 @@ const mongo = require("./database/db");
 const { auth, requiresAuth } = require("express-openid-connect");
 require("dotenv").config();
 
+//Routes imports
+const libraryRoute = require("./routes/library");
+const userRoute = require("./routes/user");
+const adminRoute = require("./routes/admin");
+const publisherRoute = require("./routes/publisher");
 
 const port = process.env.PORT || 3000;
 const app = express();
