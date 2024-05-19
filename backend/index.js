aa1690cc859066ed01345389fc8b6cf33463ed48
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-05-19 13:20:37 -0700

    Syntax

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 //Imports
-const express = require("express");
-const bodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const mongo = require("./database/db");
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongo = require('./database/db');
 
 //Authentication
-const { auth, requiresAuth } = require("express-openid-connect");
-require("dotenv").config();
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
 
 //Routes imports
-const libraryRoute = require("./routes/library");
-const userRoute = require("./routes/user");
-const adminRoute = require("./routes/admin");
-const publisherRoute = require("./routes/publisher");
+const libraryRoute = require('./routes/library');
+const userRoute = require('./routes/user');
+const adminRoute = require('./routes/admin');
+const publisherRoute = require('./routes/publisher');
 
 const port = process.env.PORT || 3000;
 const app = express();
