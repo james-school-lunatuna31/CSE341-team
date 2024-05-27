c19114cc4458ab23f9fbb959f731be104ced5ea4
Austin B Campbell/AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-05-27 15:20:04 -0700

    Testing validator helper
    Testing to see if the ISBN check function export broke things.

--- backend/helper/validator.js
+++ backend/helper/validator.js
@@ -14,58 +14,55 @@ const validator = (body, rules, customMsg, callback) => {
 /*
 * Validates an ISBN number, requires an isbn as a parameter.
 */
-const isbnValidator = (isbn, callback) => {
-    const validation = new Validator(isbn);
-    let variable = null;
+// const isbnValidator = (isbn, callback) => {
+//     const validation = new Validator(isbn);
+//     let variable = null;
 
-    isbn = isbn.replace(/-/g, '');
+//     isbn = isbn.replace(/-/g, '');
 
-    //Checks if the length of the ISBN is not 10 or 13, returns false if so.
-    if (isbn.length !== 10 && isbn.length !== 13) {
-        variable = false;
-    }
+//     //Checks if the length of the ISBN is not 10 or 13, returns false if so.
+//     if (isbn.length !== 10 && isbn.length !== 13) {
+//         variable = false;
+//     }
 
-    //10 Digit ISBN checker.
-    if (isbn.length === 10) {
-        if (!/^\d{9}(\d|X)$/.test(isbn)) {
-            variable = false;
-        }
+//     //10 Digit ISBN checker.
+//     if (isbn.length === 10) {
+//         if (!/^\d{9}(\d|X)$/.test(isbn)) {
+//             variable = false;
+//         }
 
-        let checkSum = 0;
-        for (let i = 0; i < 9; i++) {
-            checkSum += (i + 1) * parseInt(isbn[i]);
-        }
-        checkSum = checkSum % 11;
-        if (isbn[9] === 'X') {
-            variable = checkSum === 10, variable = true;
-        } else {
-            variable = checkSum === parseInt(isbn[9]);
-        }
-    }
+//         let checkSum = 0;
+//         for (let i = 0; i < 9; i++) {
+//             checkSum += (i + 1) * parseInt(isbn[i]);
+//         }
+//         checkSum = checkSum % 11;
+//         if (isbn[9] === 'X') {
+//             variable = checkSum === 10, variable = true;
+//         } else {
+//             variable = checkSum === parseInt(isbn[9]);
+//         }
+//     }
 
-    if (isbn.length === 13) {
-        if (!/^\d{13}$/.test(isbn)) {
-            variable = false;
-        }
+//     if (isbn.length === 13) {
+//         if (!/^\d{13}$/.test(isbn)) {
+//             variable = false;
+//         }
 
-        let checkSum = 0;
-        for (let i = 0; i < 12; i++) {
-            checkSum += (i % 2 === 0 ? 1 : 3) * parseInt(isbn[i]);
-        }
-        checkSum = (10 - (checkSum % 10)) % 10;
-        variable = checkSum === parseInt(isbn[12]);
-    }
+//         let checkSum = 0;
+//         for (let i = 0; i < 12; i++) {
+//             checkSum += (i % 2 === 0 ? 1 : 3) * parseInt(isbn[i]);
+//         }
+//         checkSum = (10 - (checkSum % 10)) % 10;
+//         variable = checkSum === parseInt(isbn[12]);
+//     }
 
-    if (variable == true) {
-        validation.passes(); // => callback(null, true) was in here, not working?
-        console.log("testing here in conditional.");
-    } else if (variable == false) {
-        validation.fails(() => callback(validation.errors, false));
-        console.log("Validation failed");
-    }
-};
+//     if (variable == true) {
+//         validation.passes(); // => callback(null, true) was in here, not working?
+//         console.log("testing here in conditional.");
+//     } else if (variable == false) {
+//         validation.fails(() => callback(validation.errors, false));
+//         console.log("Validation failed");
+//     }
+// };
 
-module.exports = {
-    validator, 
-    isbnValidator
-};
+module.exports = validator
